6d233f1987ad4ff1096ed5a9db92a72ba33ed03a
JVMTeixeira <joao.victormt1210@gmail.com>
2025-11-04 18:30:30 -0300

    prototipo

---
+++ calc copy.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+char input[50];
+
+int indice = 0;
+char caractere = ' ';
+
+char Numeros = {'0','1','2','3','4','5','6','7','8','9'};
+
+//pilha *pi = criar_pilha();
+
+bool ehNum(char* c){
+    if(*c == '0'||*c == '1'||*c == '2'||*c == '3'||*c == '4'||*c == '5'||*c == '6'||*c == '7'||*c == '8'||*c == '9'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int percorreEspaço(int indice){
+    int indice_temp = indice;
+    while(ehNum(input[indice_temp])){
+        indice_temp++;
+    }
+    indice_temp = indice - indice_temp;
+
+    char input_temp[indice_temp];
+    int indice_input_temp = 0;
+    for(int i = indice; i < (indice + indice_temp); i++){
+        input_temp[indice_input_temp] = input[i];
+        indice_input_temp++;
+    }
+
+    int num_push = atoi(input_temp);
+    push(*pi,num_push);
+
+    return indice + indice_temp;
+}
+
+char percorrerAte(int indice){
+
+    printf(">>> ");
+
+    if(caractere == '('){
+
+        for(int i = indice+1; i < sizeof(input);i++){
+            printf(" %c ",input[i]);
+        }
+
+    }
+
+    printf(" <<<");
+}
+
+int main(){
+
+    printf(">>> ");
+
+    fgets(input,sizeof(input),stdin);
+
+    //Ler todo o input
+    // Reconhecer cada caractere {
+    // Pegar o "(", "[" ou "{"
+    // Quando achar o primeiro símbolo, criar uma pilha.
+    // A pilha vai receber o resultado da operação que estiver dentro dela até achar a versão fechada do símbolo.
+
+
+    while(caractere != '\n'){
+
+        printf("%d- %c ",indice,input[indice]);
+
+        if(ehNum(caractere)){
+            indice = (percorreEspaço(indice));
+        }
+
+        if(caractere == '(' || caractere == '[' || caractere == '{'){
+            percorrerAte(indice); 
+        }
+
+        indice++;
+        caractere = input[indice];
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
