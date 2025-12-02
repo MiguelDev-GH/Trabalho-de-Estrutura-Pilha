75d18beaa6ad6b204fd55ae9d783d5dd79d8af2d
JVMTeixeira <joao.victormt1210@gmail.com>
2025-12-02 17:16:24 -0300

    .

--- main.c
+++ main.c
@@ -69,13 +69,15 @@ int main(){
                 break;
             }
     }
-    
+
     liberar_pilha(pi_num);
     liberar_pilha(pi_simb);
     liberar_pilha(pi_verificacao);
+    liberar_pilha(historico);
     free(pi_num);
     free(pi_simb);
     free(pi_verificacao);
+    free(historico);
 
     return 0;
 }
